1d6514a77b11d9cf0ab68c598a617ee056d496b3
Rishikesh <110241863+R-G-KJSIT@users.noreply.github.com>
2023-04-11 23:44:34 +0530

    DBMS Final

--- aircrafts.sql
+++ aircrafts.sql
@@ -30,5 +30,34 @@ CREATE TABLE CLIENTS (
     Client_name varchar(100) NOT NULL,
     Contact_email varchar(100) NOT NULL);
  
+ CREATE TABLE REQ (
+	Client_ID varchar(100) NOT NULL,
+    id varchar(100) NOT NULL);
+
+CREATE TABLE USES (
+	ManufactID varchar(100) NOT NULL,
+    id varchar(100) NOT NULL);
+
+CREATE TABLE POWER (
+	Engine_model varchar(100) NOT NULL,
+    id varchar(100) NOT NULL);
+CREATE TABLE 
+DROP TABLE BUY;
+
+DROP TABLE REQ;
+DROP TABLE USES;
+DROP TABLE POWER;
+alter table REQ add foreign key (Client_ID) references CLIENTS(Client_ID);
+alter table REQ add foreign key (id) references Components(id);
+alter table POWER add foreign key (Engine_model) references ENGINE(Engine_model);
+alter table POWER add foreign key (id) references Components(id);
+alter table USES add foreign key (ManufactID) references computer(ManufacturerID);
+alter table USES add foreign key (id) references Components(id);
 DROP TABLE Components;
 DROP TABLE COMPUTER;
+DROP TABLE ENGINE;
+
+ID varchar(100) NOT NULL,
+ENGINEID varchar(100) NOT NULL,
+
+
